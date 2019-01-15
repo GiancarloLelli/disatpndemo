2688756fd414fbefb7a084696f19205b5dc91ee5
Giancarlo Lelli <gcarlo.lelli@live.com>
2019-01-15 17:20:32 +0100

    added ble logic from netcore 3.0

--- src/DISATPN.Publisher/Program.cs
+++ src/DISATPN.Publisher/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Storage.Streams;
 
 namespace DISATPN.Publisher
 {
@@ -6,7 +11,31 @@ namespace DISATPN.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var waiter = new EventWaitHandle(false, EventResetMode.ManualReset);
+            var publisher = new BluetoothLEAdvertisementPublisher();
+
+            var manufacturerData = new BluetoothLEManufacturerData();
+            manufacturerData.CompanyId = 0xFFFE;
+
+            var writer = new DataWriter();
+            ushort uuidData = 0x1234;
+            writer.WriteUInt16(uuidData);
+
+            manufacturerData.Data = writer.DetachBuffer();
+            publisher.Advertisement.ManufacturerData.Add(manufacturerData);
+
+            publisher.StatusChanged += Publisher_StatusChanged;
+            publisher.Start();
+
+            waiter.WaitOne();
+        }
+
+        private static void Publisher_StatusChanged(BluetoothLEAdvertisementPublisher sender, BluetoothLEAdvertisementPublisherStatusChangedEventArgs args)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Status: {args.Status.ToString()}");
+            sb.AppendLine($"Error: {args.Error.ToString()}");
+            Debug.WriteLine(sb.ToString());
         }
     }
 }
